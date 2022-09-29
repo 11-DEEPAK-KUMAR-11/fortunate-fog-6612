5529c66094f76778895d9c35984dd6fc5d332c94
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2022-09-29 21:36:09 +0530

    day-2 tasks done

---
+++ Automated Auction System/src/com/masai/bean/Buyer.java
@@ -0,0 +1,42 @@
+package com.masai.bean;
+
+public class Buyer {
+
+	private String bname;
+	private String bemail;
+	private String bpassword;
+	public Buyer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Buyer(String bname, String bemail, String bpassword) {
+		super();
+		this.bname = bname;
+		this.bemail = bemail;
+		this.bpassword = bpassword;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public String getBemail() {
+		return bemail;
+	}
+	public void setBemail(String bemail) {
+		this.bemail = bemail;
+	}
+	public String getBpassword() {
+		return bpassword;
+	}
+	public void setBpassword(String bpassword) {
+		this.bpassword = bpassword;
+	}
+	@Override
+	public String toString() {
+		return "Buyer [bname=" + bname + ", bemail=" + bemail + ", bpassword=" + bpassword + "]";
+	}
+	
+	
+}
